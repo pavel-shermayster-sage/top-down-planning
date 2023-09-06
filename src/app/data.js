702a7e7a7824817a42b56b5207557f7593edca3b
gilad.hoshmand@sage.com <gilad.hoshmand@sage.com>
2023-09-06 14:18:23 +0300

    Add data leafs

--- src/app/data.js
+++ src/app/data.js
@@ -1,3 +1,225 @@
+const communications = {
+    "London - R&D - Accelerate ": 500,
+    "London - R&D - SIP": 500,
+    "London - Sales - Accelerate": 500,
+    "London - Sales - SIP": 500,
+    "London-Product-Accelerate ": 500,
+    "London Product  SIP": 500,
+    "Tel-Aviv - R&D - Accelerate ": 1000,
+    "Tel-Aviv - R&D SIP": 1000,
+    "Tel-Aviv - Sales - Accelerate": 1000,
+    "Tel-Aviv Sales - SIP": 1000,
+    "Tel-Aviv Product  - Accelerate ": 1000,
+    "Tel-Aviv Product  SIP": 1000,
+    "Chicago - R&D - Accelerate ": 600,
+    "Chicago - R&D SIP": 600,
+    "Chicago - Sales - Accelerate": 600,
+    "Chicago Sales - SIP": 600,
+    "Chicago Product  - Accelerate ": 600,
+    "Chicago Product  SIP": 600
+}
+const snacks = {
+    "London - R&D - Accelerate ": 230,
+    "London - R&D - SIP": 210,
+    "London - Sales - Accelerate": 237,
+    "London - Sales - SIP": 210,
+    "London-Product-Accelerate ": 35,
+    "London Product  SIP": 210,
+    "Tel-Aviv - R&D - Accelerate ": 279,
+    "Tel-Aviv - R&D SIP": 212,
+    "Tel-Aviv - Sales - Accelerate": 252,
+    "Tel-Aviv Sales - SIP": 212,
+    "Tel-Aviv Product  - Accelerate ": 239,
+    "Tel-Aviv Product  SIP": 212,
+    "Chicago - R&D - Accelerate ": 213,
+    "Chicago - R&D SIP": 207,
+    "Chicago - Sales - Accelerate": 227,
+    "Chicago Sales - SIP": 207,
+    "Chicago Product  - Accelerate ": 213,
+    "Chicago Product  SIP": 207
+}
+const entertainment = {
+    "London - R&D - Accelerate ": 350,
+    "London - R&D - SIP": 350,
+    "London - Sales - Accelerate": 350,
+    "London - Sales - SIP": 350,
+    "London-Product-Accelerate ": 350,
+    "London Product  SIP": 350,
+    "Tel-Aviv - R&D - Accelerate ": 350,
+    "Tel-Aviv - R&D SIP": 350,
+    "Tel-Aviv - Sales - Accelerate": 350,
+    "Tel-Aviv Sales - SIP": 350,
+    "Tel-Aviv Product  - Accelerate ": 350,
+    "Tel-Aviv Product  SIP": 350,
+    "Chicago - R&D - Accelerate ": 350,
+    "Chicago - R&D SIP": 350,
+    "Chicago - Sales - Accelerate": 350,
+    "Chicago Sales - SIP": 350,
+    "Chicago Product  - Accelerate ": 350,
+    "Chicago Product  SIP": 350
+};
+const furniture = {
+    "London - R&D - Accelerate ": 1700,
+    "London - R&D - SIP": 1700,
+    "London - Sales - Accelerate": 1700,
+    "London - Sales - SIP": 1700,
+    "London-Product-Accelerate ": 1700,
+    "London Product  SIP": 1700,
+    "Tel-Aviv - R&D - Accelerate ": 1700,
+    "Tel-Aviv - R&D SIP": 1700,
+    "Tel-Aviv - Sales - Accelerate": 1700,
+    "Tel-Aviv Sales - SIP": 1700,
+    "Tel-Aviv Product  - Accelerate ": 1700,
+    "Tel-Aviv Product  SIP": 1700,
+    "Chicago - R&D - Accelerate ": 1700,
+    "Chicago - R&D SIP": 1700,
+    "Chicago - Sales - Accelerate": 1700,
+    "Chicago Sales - SIP": 1700,
+    "Chicago Product  - Accelerate ": 1700,
+    "Chicago Product  SIP": 1700
+};
+const computers = {
+    "London - R&D - Accelerate ": 3300,
+    "London - R&D - SIP": 3000,
+    "London - Sales - Accelerate": 3400,
+    "London - Sales - SIP": 3000,
+    "London-Product-Accelerate ": 380,
+    "London Product  SIP": 3000,
+    "Tel-Aviv - R&D - Accelerate ": 4000,
+    "Tel-Aviv - R&D SIP": 3000,
+    "Tel-Aviv - Sales - Accelerate": 3600,
+    "Tel-Aviv Sales - SIP": 3000,
+    "Tel-Aviv Product  - Accelerate ": 3400,
+    "Tel-Aviv Product  SIP": 3000,
+    "Chicago - R&D - Accelerate ": 3100,
+    "Chicago - R&D SIP": 3000,
+    "Chicago - Sales - Accelerate": 3300,
+    "Chicago Sales - SIP": 3000,
+    "Chicago Product  - Accelerate ": 3100,
+    "Chicago Product  SIP": 3000
+};
+const flights = {
+    "London - R&D - Accelerate ": 2000,
+    "London - R&D - SIP": 2000,
+    "London - Sales - Accelerate": 7000,
+    "London - Sales - SIP": 6000,
+    "London-Product-Accelerate ": 3500,
+    "London Product  SIP": 3500,
+    "Tel-Aviv - R&D - Accelerate ": 2000,
+    "Tel-Aviv - R&D SIP": 2000,
+    "Tel-Aviv - Sales - Accelerate": 7000,
+    "Tel-Aviv Sales - SIP": 6000,
+    "Tel-Aviv Product  - Accelerate ": 3500,
+    "Tel-Aviv Product  SIP": 3500,
+    "Chicago - R&D - Accelerate ": 2200,
+    "Chicago - R&D SIP": 2100,
+    "Chicago - Sales - Accelerate": 7500,
+    "Chicago Sales - SIP": 6400,
+    "Chicago Product  - Accelerate ": 3800,
+    "Chicago Product  SIP": 3600
+};
+const conferences = {
+    "London - R&D - Accelerate ": 400,
+    "London - R&D - SIP": 350,
+    "London - Sales - Accelerate": 1700,
+    "London - Sales - SIP": 1800,
+    "London-Product-Accelerate ": 600,
+    "London Product  SIP": 600,
+    "Tel-Aviv - R&D - Accelerate ": 300,
+    "Tel-Aviv - R&D SIP": 300,
+    "Tel-Aviv - Sales - Accelerate": 1500,
+    "Tel-Aviv Sales - SIP": 1500,
+    "Tel-Aviv Product  - Accelerate ": 450,
+    "Tel-Aviv Product  SIP": 450,
+    "Chicago - R&D - Accelerate ": 300,
+    "Chicago - R&D SIP": 300,
+    "Chicago - Sales - Accelerate": 1500,
+    "Chicago Sales - SIP": 1500,
+    "Chicago Product  - Accelerate ": 450,
+    "Chicago Product  SIP": 450
+};
+const hotels=    {
+    "London - R&D - Accelerate ": 800,
+    "London - R&D - SIP": 800,
+    "London - Sales - Accelerate": 2500,
+    "London - Sales - SIP": 2500,
+    "London-Product-Accelerate ": 1200,
+    "London Product  SIP": 1200,
+    "Tel-Aviv - R&D - Accelerate ": 1000,
+    "Tel-Aviv - R&D SIP": 900,
+    "Tel-Aviv - Sales - Accelerate": 3000,
+    "Tel-Aviv Sales - SIP": 3100,
+    "Tel-Aviv Product  - Accelerate ": 1400,
+    "Tel-Aviv Product  SIP": 1500,
+    "Chicago - R&D - Accelerate ": 800,
+    "Chicago - R&D SIP": 800,
+    "Chicago - Sales - Accelerate": 2500,
+    "Chicago Sales - SIP": 2500,
+    "Chicago Product  - Accelerate ": 1200,
+    "Chicago Product  SIP": 1200
+};
+const insurance   = {
+
+    "London - R&D - Accelerate ": 1150,
+    "London - R&D - SIP": 1050,
+    "London - Sales - Accelerate": 1183,
+    "London - Sales - SIP": 1050,
+    "London-Product-Accelerate ": 177,
+    "London Product  SIP": 1050,
+    "Tel-Aviv - R&D - Accelerate ": 1393,
+    "Tel-Aviv - R&D SIP": 1060,
+    "Tel-Aviv - Sales - Accelerate": 1260,
+    "Tel-Aviv Sales - SIP": 1060,
+    "Tel-Aviv Product  - Accelerate ": 1193,
+    "Tel-Aviv Product  SIP": 1060,
+    "Chicago - R&D - Accelerate ": 1067,
+    "Chicago - R&D SIP": 1033,
+    "Chicago - Sales - Accelerate": 1133,
+    "Chicago Sales - SIP": 1033,
+    "Chicago Product  - Accelerate ": 1067,
+    "Chicago Product  SIP": 1033
+}
+const salary = {
+    "London - R&D - Accelerate ": 33000,
+    "London - R&D - SIP": 30000,
+    "London - Sales - Accelerate": 34000,
+    "London - Sales - SIP": 30000,
+    "London-Product-Accelerate ": 3800,
+    "London Product  SIP": 30000,
+    "Tel-Aviv - R&D - Accelerate ": 40000,
+    "Tel-Aviv - R&D SIP": 30000,
+    "Tel-Aviv - Sales - Accelerate": 36000,
+    "Tel-Aviv Sales - SIP": 30000,
+    "Tel-Aviv Product  - Accelerate ": 34000,
+    "Tel-Aviv Product  SIP": 30000,
+    "Chicago - R&D - Accelerate ": 31000,
+    "Chicago - R&D SIP": 30000,
+    "Chicago - Sales - Accelerate": 33000,
+    "Chicago Sales - SIP": 30000,
+    "Chicago Product  - Accelerate ": 31000,
+    "Chicago Product  SIP": 30000
+}
+const benefits = {
+    "London - R&D - Accelerate ": 1500,
+    "London - R&D - SIP": 1500,
+    "London - Sales - Accelerate": 1500,
+    "London - Sales - SIP": 1500,
+    "London-Product-Accelerate ": 1500,
+    "London Product  SIP": 1500,
+    "Tel-Aviv - R&D - Accelerate ": 1800,
+    "Tel-Aviv - R&D SIP": 1800,
+    "Tel-Aviv - Sales - Accelerate": 1800,
+    "Tel-Aviv Sales - SIP": 1800,
+    "Tel-Aviv Product  - Accelerate ": 1800,
+    "Tel-Aviv Product  SIP": 1800,
+    "Chicago - R&D - Accelerate ": 1000,
+    "Chicago - R&D SIP": 1000,
+    "Chicago - Sales - Accelerate": 1000,
+    "Chicago Sales - SIP": 1000,
+    "Chicago Product  - Accelerate ": 1000,
+    "Chicago Product  SIP": 1000
+}
+
 export const data = [
     {
         expenseId: 0,
@@ -12,35 +234,35 @@ export const data = [
                 {
                     expenseId: 11,
                     expenseName: "Communications",
-                    expenseAmount: {},
+                    expenseAmount: communications,
                     sibling: [],
                     total: 12600
                 },
                 {
                     expenseId: 12,
                     expenseName: "Snacks",
-                    expenseAmount: {},
+                    expenseAmount: snacks,
                     sibling: [],
                     total: 3812
                 },
                 {
                     expenseId: 13,
                     expenseName: "Entertainment",
-                    expenseAmount: {},
+                    expenseAmount: entertainment,
                     sibling: [],
                     total: 6300
                 },
                 {
                     expenseId: 14,
                     expenseName: "Furniture",
-                    expenseAmount: {},
+                    expenseAmount: furniture,
                     sibling: [],
                     total: 30600
                 },
                 {
                     expenseId: 15,
                     expenseName: "Computers",
-                    expenseAmount: {},
+                    expenseAmount: computers,
                     sibling: [],
                     total: 54580
                 },
@@ -55,21 +277,21 @@ export const data = [
                     {
                         expenseId: 21,
                         expenseName: "Flights",
-                        expenseAmount: {},
+                        expenseAmount: flights,
                         sibling: [],
                         total: 73600
                     },
                     {
                         expenseId: 22,
                         expenseName: "Conferences",
-                        expenseAmount: {},
+                        expenseAmount: conferences,
                         sibling: [],
                         total: 14450
                     },
                     {
                         expenseId: 23,
                         expenseName: "Hotels",
-                        expenseAmount: {},
+                        expenseAmount: hotels,
                         sibling: [],
                         total: 28900
                     },
@@ -84,21 +306,21 @@ export const data = [
                     {
                         expenseId: 31,
                         expenseName: "Insurance",
-                        expenseAmount: {},
+                        expenseAmount: insurance,
                         sibling: [],
                         total: 19052
                     },
                     {
                         expenseId: 32,
                         expenseName: "Salary",
-                        expenseAmount: {},
+                        expenseAmount: salary,
                         sibling: [],
                         total: 545800
                     },
                     {
                         expenseId: 33,
                         expenseName: "Benefits",
-                        expenseAmount: {},
+                        expenseAmount: benefits,
                         sibling: [],
                         total: 25800
                     },
