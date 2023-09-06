c99e76e0ce6f384f0421a8add9bbd1b4829871f4
Pavel Shermayster <pavel.shermayste@sage.com>
2023-09-06 14:15:27 +0300

    little style improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,64 +12,97 @@ export default function Home() {
   const onChange = (item: any, value: any) => {
     console.log("value", { value, item });
     item.percentage = value; // does not exist
-	};
-	
-	const onSelectChange = (event: any) => {
+  };
+
+  const onSelectChange = (event: any) => {
     console.log(event.target.id, event.target.value);
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col justify-between p-24">
       <div>
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <h1 className="mb-4 text-center text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
           Budget{" "}
           <span className="text-blue-600 dark:text-blue-500"> calculator</span>{" "}
         </h1>
         <div className="flex flex-col items-center justify-center">
-				<div className="flex gap-2">
-						<Select id="department" onChange={onSelectChange} items={[{ value: "rnd", label: "R&D" }, { value: "sales", label: "Sales"}, { value: "product", label: "Product"}]}></Select>
-						<Select id="location" onChange={onSelectChange} items={[{ value: "london", label: "London" }, { value: "tel-aviv", label: "Tel Aviv"}, { value: "chicago", label: "Chicago"}]}></Select>
-						<Select id="project" onChange={onSelectChange} items={[{ value: "accelerator", label: "Accelerator" }, { value: "sip", label: "SIP"}]}></Select>
-					</div>
-          {data.map((item) => {
-            return (
-              <>
-                <Section
-                  key={item.expenseId}
-                  item={item}
-                  onChange={onChange}
-                  onLockChange={(isLocked: boolean) => {
-                    console.log("isLocked", isLocked);
-                  }}
-                />
-                {item.sibling.map((item) => {
-                  return (
-                    <div key={item.expenseId}>
-                      <Section
-                        item={item}
-                        onChange={onChange}
-                        onLockChange={(isLocked: boolean) => {
-                          console.log("isLocked", isLocked);
-                        }}
-                      />
-                      {item.sibling.map((item) => {
-                        return (
-                          <Section
-                            key={item.expenseId}
-                            item={item}
-                            onChange={onChange}
-                            onLockChange={(isLocked: boolean) => {
-                              console.log("isLocked", isLocked);
-                            }}
-                          />
-                        );
-                      })}
-                    </div>
-                  );
-                })}
-              </>
-            );
-          })}
+          <div className="flex gap-2">
+            <Select
+              id="department"
+              onChange={onSelectChange}
+              items={[
+                { value: "rnd", label: "R&D" },
+                { value: "sales", label: "Sales" },
+                { value: "product", label: "Product" },
+              ]}
+            ></Select>
+            <Select
+              id="location"
+              onChange={onSelectChange}
+              items={[
+                { value: "london", label: "London" },
+                { value: "tel-aviv", label: "Tel Aviv" },
+                { value: "chicago", label: "Chicago" },
+              ]}
+            ></Select>
+            <Select
+              id="project"
+              onChange={onSelectChange}
+              items={[
+                { value: "accelerator", label: "Accelerator" },
+                { value: "sip", label: "SIP" },
+              ]}
+            ></Select>
+          </div>
+          <div className="flex flex-col gap-1 items-start mt-10 border p-10 rounded">
+            {data.map((item) => {
+              return (
+                <>
+                  <Section
+                    key={item.expenseId}
+                    item={item}
+                    onChange={onChange}
+                    onLockChange={(isLocked: boolean) => {
+                      console.log("isLocked", isLocked);
+                    }}
+                  />
+                  {item.sibling.map((item) => {
+                    return (
+                      <div
+                        className="ml-6 flex flex-col gap-1 items-start"
+                        key={item.expenseId}
+                      >
+                        <Section
+                          item={item}
+                          onChange={onChange}
+                          onLockChange={(isLocked: boolean) => {
+                            console.log("isLocked", isLocked);
+                          }}
+                        />
+                        {item.sibling.map((item) => {
+                          return (
+                            <div
+                              className="ml-6 flex flex-col gap-1 items-start"
+                              key={item.expenseId}
+                            >
+                              <Section
+                                key={item.expenseId}
+                                item={item}
+                                onChange={onChange}
+                                onLockChange={(isLocked: boolean) => {
+                                  console.log("isLocked", isLocked);
+                                }}
+                              />
+                            </div>
+                          );
+                        })}
+                      </div>
+                    );
+                  })}
+                </>
+              );
+            })}
+          </div>
         </div>
       </div>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -15,26 +15,24 @@ export function Section({
 }) {
   const [locked, setLocked] = useState(false);
   return (
-    <div className="flex flex-col">
-      <div className="flex gap-8 align-sub">
-        <div>Total: {item.total}</div>
-        <div>Name: {item.expenseName}</div>
-        <Slider
-          id="slider"
-          disabled={locked}
-          onChange={(_, value) => {
-            onChange(item, value);
-          }}
-        />
-        <button
-          onClick={() => {
-            setLocked((s) => !s);
-            onLockChange(item, !locked);
-          }}
-        >
-          {locked ? <UnLockIcon /> : <LockIcon />}
-        </button>
-      </div>
+    <div className="flex justify-between gap-8 items-center h-20 w-full">
+      <span className="font-bold">{item.expenseName}</span>
+      <div>{item.total}</div>
+      <Slider
+        id="slider"
+        disabled={locked}
+        onChange={(_, value) => {
+          onChange(item, value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setLocked((s) => !s);
+          onLockChange(item, !locked);
+        }}
+      >
+        {locked ? <UnLockIcon /> : <LockIcon />}
+      </button>
     </div>
   );
 }
