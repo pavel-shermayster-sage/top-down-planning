22f8f7793ecd3e78bab7be30019fce259ba6bf16
Pavel Shermayster <pavel.shermayste@sage.com>
2023-09-06 13:16:51 +0300

    move to section

---
+++ src/components/lock-svg.tsx
@@ -0,0 +1,4 @@
+import Image from "next/image";
+export function LockIcon() {
+  return <Image src="/lock.svg" alt="Lock" />;
+}
---
+++ src/components/section.tsx
@@ -0,0 +1,24 @@
+import { Slider } from "./slider";
+
+// type Item = {
+//   total: number;
+//   expenseName: string;
+//   expenseId: string;
+//   percentage: number;
+// };
+export function Section({ item, onChange }: { item: any; onChange: any }) {
+  return (
+    <div className="flex flex-col">
+      <div className="flex gap-8">
+        <div>Total: {item.total}</div>
+        <div>Name: {item.expenseName}</div>
+        <Slider
+          id="slider"
+          onChange={(_, value) => {
+            onChange(item, value);
+          }}
+        />
+      </div>
+    </div>
+  );
+}
